d8c45b9a188e8562700af5a121cecf793d68de6e
Freeman <swissarthurfreeman@gmail.com>
2024-03-20 16:13:03 +0100

    feat(refero:web-ui): added foreign key column config fields in ref creation

--- web-ui/src/app/components/ref-creation/ref-creation.component.ts
+++ web-ui/src/app/components/ref-creation/ref-creation.component.ts
@@ -3,7 +3,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
-import { ActivatedRoute, Router } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatGridListModule} from '@angular/material/grid-list';
@@ -11,30 +10,53 @@ import {MatInputModule} from '@angular/material/input';
 import { ViewEditorComponent } from "../view-editor/view-editor.component";
 import { SearchComponent } from "../search/search.component";
 import { TableComponent } from "../table/table.component";
-import { ColConfigComponent } from "../col-config/col-config.component";
-import { RefDataService } from '../../service/ref-data.service';
-import { FormArray, FormBuilder, FormControl } from '@angular/forms';
+import { FormArray, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-ref-creation',
     standalone: true,
     templateUrl: './ref-creation.component.html',
-    imports: [MatTableModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatDividerModule, MatIconModule, MatGridListModule, MatInputModule, ViewEditorComponent, SearchComponent, TableComponent, ColConfigComponent]
+    imports: [CommonModule, MatTableModule, ReactiveFormsModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatDividerModule, MatIconModule, MatGridListModule, MatInputModule, ViewEditorComponent, SearchComponent, TableComponent]
 })
 export class RefCreationComponent implements OnInit {
-  constructor() {
+[x: string]: any;
+  RefConfigForm = this.fb.group({
+      columns: this.fb.array([])
+  });
 
-  }
-
-  dataService = inject(RefDataService);
-
-  fileHeader: Array<string> = ['Code', 'Parent', 'Level', 'Name_fr', 'Desc_fr'];
-
-  newHeader: Array<string> = [];
-  AddCol(event: any) {
-    this.newHeader.push("");
-  }
-
+  constructor(private fb:FormBuilder) {}
+  
+  FileCols: Array<string> = ['Code', 'Parent', 'Level', 'Name_fr', 'Desc_fr'];
   ngOnInit(): void {
-  }  
+    for(let col of this.FileCols) {
+      this.AddCol(col);
+    }
+  }
+
+  get columns() {
+    return this.RefConfigForm.controls["columns"] as FormArray;
+  }
+
+  AddCol(name?: string) {
+    const ColForm = this.fb.group({
+      RefCol: ['' || name?.toUpperCase(), Validators.required],
+      FileCol: ['' || name ],
+      DateFormat: [''],
+      Required: ["true"],
+      KeyType: [''],
+      PointedRef: [''],
+      PointedCol: [''],
+      LabelCol: ['']
+    });
+    this.columns.push(ColForm);
+  }
+
+  deleteColumn(idx: number) {
+    this.columns.removeAt(idx);
+  }
+
+  Debug() {
+    console.log(this.RefConfigForm.getRawValue());
+  }
 }
