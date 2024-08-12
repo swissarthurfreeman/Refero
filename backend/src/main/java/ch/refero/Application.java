c4f9ab1900dfc87e9d47e5fda438e501fad8edbf
swissarthurfreeman <swissarthurfreeman@gmail.com>
2024-08-12 12:23:39 +0200

    feat(refero:backend): Add basic LDAP authentication endpoint.

--- backend/src/main/java/ch/refero/Application.java
+++ backend/src/main/java/ch/refero/Application.java
@@ -1,7 +1,10 @@
 package ch.refero;
+import java.net.URI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,8 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class Application {
 	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
+	ResponseEntity<Void> authenticate() {
+		return ResponseEntity.status(HttpStatus.FOUND)
+				.location(URI.create("http://localhost:4200"))
+				.build();
 	}
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/ch/refero/authentication/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package ch.refero.authentication;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults());
+
+    //http.csrf().disable();
+    return http.build();
+  }
+
+  @Autowired
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        .ldapAuthentication()
+        .userDnPatterns("uid={0},ou=people")
+        .groupSearchBase("ou=groups")
+        .contextSource()
+        .url("ldap://localhost:8389/dc=unige,dc=ch")
+        .and()
+        .passwordCompare()
+        .passwordEncoder(new BCryptPasswordEncoder())
+        .passwordAttribute("userPassword");
+  }
+}
